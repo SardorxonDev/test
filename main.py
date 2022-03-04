e71308571d3753300d78064ff32ae5eff6dd7ddf
SardorxonDev <99531265+SardorxonDev@users.noreply.github.com>
2022-03-04 13:25:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,184 @@
-from pyrogram import Client
-from pyrogram.filters import command, private, text
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InlineQueryResultArticle, InputTextMessageContent
+import os
+import time
+import secrets
+import requests
+import telebot
+import py_compile
+import random
+import json
+import base64
+import marshal
+import zlib
+from telebot import *
+from telebot import util
+from telebot import types
+import user_agent
+tokin = "5190512155:AAFB1_YRSPb3Hxi17F6ntkKMBMMzwYg2DOY" 
 
-from function import transliterate
-
-app = Client("tilon", api_id=2424010, api_hash='9fb173ded391c8aca77667709be270e3',
-             bot_token="1940937396:AAHU2Xr7Vwp5K_MNciGqyK0btrcGMYDmNrA")
-
-
-@app.on_message(private & command(['start']))
-async def _start(_, message):
-    keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("Inlineda sinab ko'rish", switch_inline_query="Salom")]])
-
-    await message.reply_text("Assalomu alaykum xush kelibsiz\n\nBot orqali siz lotin xafridagi soʻzlar, gaplarni kirill xarfiga va kirill xarfidagi soʻzlar gaplarni lotinga oʻgirib beraman!\n\n👨‍💻Admin : @Py_run\n\n🤖Bot : @Kirill_lotin_robot", reply_markup=keyboard)
-
-
-@app.on_message(private & text)
-async def hello(_, message):
-    await message.reply_text(transliterate(message.text))
-
-
-@app.on_inline_query()
-async def _inline(_, query):
-    try:
-        await query.answer(results=[
-            InlineQueryResultArticle(
-                title="Yuborish",
-                input_message_content=InputTextMessageContent(
-                    transliterate(query.query)
-                ),
-            )
-        ], cache_time=0)
-    except:
-        await query.answer(results=[
-            InlineQueryResultArticle(
-                title="Xatolik",
-                input_message_content=InputTextMessageContent(
-                    "Qandaydir xatolik bo'ldi\n@TILON bilan bog'laning."
-                ),
-            )
-        ], cache_time=0)
+def check_user(user_id):
+    global tokin
+    request = requests.get(f"https://api.telegram.org/bot{tokin}/getChatMember?chat_id=@TezkorXabarlar_Olami&user_id={user_id}").text
+    if '"status":"left"' in request or '"Bad Request: USER_ID_INVALID"' in request or '"status":"kicked"' in request or 'user not found' in request:
+        return False
     else:
-        await query.answer(results=[])
+        return True
 
 
-app.run()
+
+bot = telebot.TeleBot(tokin)
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    channel = types.InlineKeyboardButton(text=" Kanalimiz Developer ", url=f"https://t.me/TezkorXabarlar_Olami")
+    if check_user(message.from_user.id):
+        start = types.InlineKeyboardButton(text="Gif yasash ", callback_data="Start")
+        programmer = types.InlineKeyboardButton(text=" 👨🏻‍💻 Developer ", url=f"https://t.me/py_run")
+        Keyboards = types.InlineKeyboardMarkup()
+        Keyboards.row_width = 2
+        Keyboards.add(start, programmer, channel)
+        bot.send_message(message.chat.id, text=f"Assalomu alaykum {message.from_user.first_name} Sizni korganimizdan hursandmiz O'zingizga kerakli bo'limni tanlang", reply_to_message_id=(message.message_id), reply_markup=Keyboards)
+    else:
+        Keyboard = types.InlineKeyboardMarkup()
+        Keyboard.row_width = 1
+        Keyboard.add(channel)
+        
+        bot.reply_to(message, text=f"💖| Assalomu Alaykum {message.from_user.first_name} \n🔰| Hush kelibsiz botimizdan foydalanish uchun kanalimizga A'zo bolib qayta /start bosing", reply_markup=Keyboard)
+        
+def start(message):
+    channel = types.InlineKeyboardButton(text="Kanalimiz ", url=f"https://t.me/off_python")
+    programmer = types.InlineKeyboardButton(text=" 👨🏻‍💻 admin ", url=f"https://t.me/py_run")
+    Key = types.InlineKeyboardMarkup()
+    Key.row_width = 2
+    Key.add(channel, programmer)
+    bot.edit_message_text(text=f"""💖| Assalom {message.from_user.first_name} O'zingizga kerakli bo'limni tanlang
+◆━━━━━◆ ✲ ◆━━━━━◆
+ 1 )Gif yasash >>> /gif
+ 2 ) info Name >>> /info
+ 
+◆━━━━━◆ ✲ ◆━━━━━◆""", chat_id=int(message.chat.id), message_id=message.message_id, reply_markup=Key)
+
+
+
+
+@bot.message_handler(commands=['info'])
+def info_get(message):
+    channel = types.InlineKeyboardButton(text=" Kanalimiz ", url=f"https://t.me/off_python")
+    programmer = types.InlineKeyboardButton(text=" 👨🏻‍💻 Admin ", url=f"https://t.me/py_run")
+    Inline = types.InlineKeyboardMarkup()
+    Inline.row_width = 2
+    Inline.add(channel, programmer)
+    bot.send_photo(message.chat.id,f"https://t.me/{message.from_user.username}",f"""
+◆━━━━━◆ ✲ ◆━━━━━◆
+✲ ID : `{message.from_user.id}`
+◆━━━━━◆ ✲ ◆━━━━━◆
+✲ Name : `{message.chat.first_name}`
+◆━━━━━◆ ✲ ◆━━━━━◆
+✲ user : `@{message.from_user.username}`
+◆━━━━━◆ ✲ ◆━━━━━◆""", parse_mode="markdown", reply_markup=Inline)
+
+@bot.message_handler(commands=['gif'])
+def info_get(message):
+    channel = types.InlineKeyboardButton(text=" Developer ", url=f"https://t.me/py_run")
+    programmer = types.InlineKeyboardButton(text=" 👨🏻‍💻 Developer ", url=f"https://t.me/Off_python")
+    Inline = types.InlineKeyboardMarkup()
+    Inline.row_width = 2
+    Inline.add(channel, programmer)
+    bot.send_photo(message.chat.id,f"https://t.me/{message.from_user.username}",f"""{message.from_user.first_name} O'z ismingizga Gif yasash uchun ismingizni menga yuboring""", parse_mode="markdown", reply_markup=Inline)
+
+
+
+
+@bot.message_handler(func=lambda m:True)
+def mgit(message):
+    text = message.text
+    url = 'https://cooltext.com/PostChange'
+    headers = {'accept': '*/*', 'accept-encoding': 'gzip, deflate, br', 'accept-language': 'en-US,en;q=0.9',
+               'content-type': 'application/x-www-form-urlencoded; charset=UTF-8', 'origin': 'https://cooltext.com',
+               'referer': 'https://cooltext.com/Logo-Design-Animated-Glow', 'sec-fetch-dest': 'empty',
+               'sec-fetch-mode': 'cors', 'sec-fetch-site': 'same-origin', 'sec-gpc': '1',
+               'user-agent': user_agent.generate_user_agent(), 'x-requested-with': 'XMLHttpRequest'}
+    data = {
+        'LogoID': '26',
+        'Text': f'{text}',
+        'FontSize': '120',
+        'Color1_color': '#000000', 'Color2_color': '#FFFFFF', 'Color3_color': '#000000',
+        'Integer9': '0', 'Integer13': 'on', 'Integer12': 'on',
+        'BackgroundColor_color': '#FFFFFF'
+    }
+    r = requests.post(url, headers=headers, data=data)
+    logo1 = (r.json()['renderLocation'])
+    data = {
+        'LogoID': '4',
+        'Text': f'{text}',
+        'FontSize': '120',
+        'Color1_color': '#000000', 'Color2_color': '#FFFFFF', 'Color3_color': '#000000',
+        'Integer9': '0', 'Integer13': 'on', 'Integer12': 'on',
+        'BackgroundColor_color': '#FFFFFF'
+    }
+    r = requests.post(url, headers=headers, data=data)
+    logo2 = (r.json()['renderLocation'])
+    data = {
+        'LogoID': '819721038',
+        'Text': f'{text}',
+        'FontSize': '120',
+        'Color1_color': '#000000', 'Color2_color': '#FFFFFF', 'Color3_color': '#000000',
+        'Integer9': '0', 'Integer13': 'on', 'Integer12': 'on',
+        'BackgroundColor_color': '#FFFFFF'
+    }
+    r = requests.post(url, headers=headers, data=data)
+    logo3 = (r.json()['renderLocation'])
+    data = {
+        'LogoID': '1169711118',
+        'Text': f'{text}',
+        'FontSize': '120',
+        'Color1_color': '#000000', 'Color2_color': '#FFFFFF', 'Color3_color': '#000000',
+        'Integer9': '0', 'Integer13': 'on', 'Integer12': 'on',
+        'BackgroundColor_color': '#FFFFFF'
+    }
+    r = requests.post(url, headers=headers, data=data)
+    logo4 = (r.json()['renderLocation'])
+    data = {
+        'LogoID': '819515844',
+        'Text': f'{text}',
+        'FontSize': '120',
+        'Color1_color': '#000000', 'Color2_color': '#FFFFFF', 'Color3_color': '#000000',
+        'Integer9': '0', 'Integer13': 'on', 'Integer12': 'on',
+        'BackgroundColor_color': '#FFFFFF'
+    }
+    r = requests.post(url, headers=headers, data=data)
+    logo5 = (r.json()['renderLocation'])
+    data = {
+        'LogoID': '790967832',
+        'Text': f'{text}',
+        'FontSize': '120',
+        'Color1_color': '#000000', 'Color2_color': '#FFFFFF', 'Color3_color': '#000000',
+        'Integer9': '0', 'Integer13': 'on', 'Integer12': 'on',
+        'BackgroundColor_color': '#FFFFFF'
+    }
+    r = requests.post(url, headers=headers, data=data)
+    logo6 = (r.json()['renderLocation'])
+    bot.send_video(message.chat.id,logo1)
+    bot.send_video(message.chat.id,logo2)
+    bot.send_video(message.chat.id,logo3)
+    bot.send_video(message.chat.id,logo4)
+    bot.send_video(message.chat.id,logo5)
+    bot.send_video(message.chat.id,logo6)
+    bot.send_message(message.chat.id,f"Done!")
+    pass
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callbacks_data(call):
+    if call.data == "Start":
+        start(call.message)
+    if call.data == "gif":
+        sal(call.message)
+
+while True:
+    try:
+        print("Done")
+        bot.polling(True)
+        break
+    except Exception as ex:
+        print(f"Error polling : {ex}")
+        telebot.logger.error(ex)
+        continue
